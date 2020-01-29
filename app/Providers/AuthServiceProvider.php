b85218141a9971d0c39ce191b3025db3d0ac7d10
Lyn <wenzhan.main@gmail.com>
2020-01-28 17:30:44 -0800

    passport adaptation

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\Auth\CacheUserProvider;
@@ -30,5 +31,7 @@ class AuthServiceProvider extends ServiceProvider
         Auth::provider('cache-user', function() {
             return resolve(CacheUserProvider::class);
         });
+
+        Passport::routes();
     }
 }
